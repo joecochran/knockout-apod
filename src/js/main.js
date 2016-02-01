ad0b7d11382d7d41a22b060e82169ed4d5caa684
Joe Cochran <j_cochran@me.com>
2016-01-31 16:53:06 -0800

    DRYing things up

--- src/js/main.js
+++ src/js/main.js
@@ -7,11 +7,16 @@
         // https://api.nasa.gov/planetary/apod?date=YYYY-MM-DD
         // if only year, go to Jan 1 of that year
         // if only year and month, go to the first of that month
-
+function apodDate(date) {
+    var yyyy = date.getFullYear().toString();
+    var mm = (date.getMonth() + 1).toString();
+    var dd = date.getDate().toString();
+    return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
+}
 function ApodViewModel() {
     var self = this;
 
-    self.key = 'DEMO_KEY';
+    self.key = 'DEMO_KEY'; //change to DEMO_KEY before comitting
 
     self.copyright = ko.observable();
     self.date = ko.observable();
@@ -19,84 +24,40 @@ function ApodViewModel() {
     self.url = ko.observable();
     self.title = ko.observable();
     self.mediaType = ko.observable();
-
-    self.getLatest = function() {
-        $.getJSON('https://api.nasa.gov/planetary/apod?api_key=' + self.key, function(data){
-            self.copyright(data.copyright);
-            self.date(data.date);
-            self.explanation(data.explanation);
-            self.url(data.url);
-            self.title(data.title);
-            self.mediaType(data.media_type);
-        });
-    }
+    
     self.prettyDate = ko.computed(function() {
-        
+        if (self.date() instanceof Date) {
+            return self.date().toDateString();
+        }
     });
     self.prevDate = ko.computed(function(){
-        var dateString = self.date();
-        if (typeof dateString == "string") {
-            var arr = dateString.split('-').join('/');
-            var date = new Date(arr);
+        if (self.date() instanceof Date) {
+            date = new Date(self.date().toString());
             date.setDate(date.getDate() - 1);
-            var yyyy = date.getFullYear().toString();
-            var mm = (date.getMonth() + 1).toString();
-            var dd = date.getDate().toString();
-            return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
+            return apodDate(date);
         }
-        return false;
     });
     self.nextDate = ko.computed(function(){
-        var dateString = self.date();
-        if (typeof dateString == "string") {
-            var arr = dateString.split('-').join('/');
-            var date = new Date(arr);
+        if (self.date() instanceof Date) {
+            date = new Date(self.date().toString());
             date.setDate(date.getDate() + 1);
-            if (date > new Date()) {
-                return false;
-            }
-            var yyyy = date.getFullYear().toString();
-            var mm = (date.getMonth() + 1).toString();
-            var dd = date.getDate().toString();
-            return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
+            return (date < new Date()) ? apodDate(date) : false;
         }
-        return false;
     });
-    self.getPrevious = function() {
-        $.getJSON('https://api.nasa.gov/planetary/apod?api_key=' + self.key + '&date=' + self.prevDate(), function(data){
-            self.copyright(data.copyright);
-            self.date(data.date);
-            self.explanation(data.explanation);
-            self.url(data.url);
-            self.title(data.title);
-            self.mediaType(data.media_type);
-        });
-    }
-    self.getNext = function() {
-        $.getJSON('https://api.nasa.gov/planetary/apod?api_key=' + self.key + '&date=' + self.nextDate(), function(data){
-            self.copyright(data.copyright);
-            self.date(data.date);
-            self.explanation(data.explanation);
-            self.url(data.url);
-            self.title(data.title);
-            self.mediaType(data.media_type);
-        });
-    }
+    self.latest = apodDate(new Date());
+
     self.getDate = function(date) {
+        $.getJSON('https://api.nasa.gov/planetary/apod?api_key=' + self.key + '&date=' + date, function(data){
+            self.copyright(data.copyright);
+            self.date( new Date(data.date.split('-').join('/')));
+            self.explanation(data.explanation);
+            self.url(data.url);
+            self.title(data.title);
+            self.mediaType(data.media_type);
+        });
     }
     
     // get the most recent one right away
-    self.getLatest();
+    self.getDate(self.latest);
 }
 ko.applyBindings(new ApodViewModel());
-function makeDate(date) {
-    var arr = date.split('-').join('/');
-    // var arr = date.split('-');
-    // var year = parseInt(arr[0]).toString();
-    // var month = parseInt(arr[1]).toString();
-    // var day = parseInt(arr[2]).toString();
-    // console.log(year + ' ' + month + ' ' + day);
-    var date = new Date(arr);
-    return date;
-
-}
